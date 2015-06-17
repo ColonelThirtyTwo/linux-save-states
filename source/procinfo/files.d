924bd3ddc3c371b87c731bd55995ff290610a58b
Alex Parrill <initrd.gz@gmail.com>
2015-06-17 15:40:00 -0400

    Don't close files that can't be saved when loading a new state.

--- source/procinfo/files.d
+++ source/procinfo/files.d
@@ -8,7 +8,7 @@ import std.string : chomp, chompPrefix;
 import std.algorithm;
 import std.range;
 import std.regex;
-import std.typecons : Nullable;
+import std.typecons : Nullable, Tuple, tuple;
 import std.c.linux.linux : pid_t;
 
 import models;
@@ -16,32 +16,20 @@ import procinfo;
 import procinfo.cmdpipe;
 import procinfo.tracer : WaitEvent;
 
+private {
+	alias specialLinkRE = ctRegex!`^([a-zA-Z0-9_]*):\[?([^\]]+)\]?$`;
+	alias fdInfoRE = ctRegex!(`^pos:\s+([0-9]+)\s+flags:\s+([0-9]+)\s*$`);
+}
+
 /// Reads all of the file descriptors of a process and returns a range of FileDescriptor structs.
 /// The process should be paused during this.
 /// Stdin/out/err and the command pipes are excluded from being saved.
 auto readFiles(pid_t pid) {
-	// TODO: replace stderr.writeln with logging
-	alias specialLinkRE = ctRegex!`^([a-zA-Z0-9_]*):\[?([^\]]+)\]?$`;
-	alias fdInfoRE = ctRegex!(`^pos:\s+([0-9]+)\s+flags:\s+([0-9]+)\s*$`);
-	
-	return dirEntries("/proc/"~to!string(pid)~"/fd/", SpanMode.shallow)
-		.map!(delegate(DirEntry dir) {
-			int fd = dir.name.findSplitAfter("/fd/")[1].to!int;
-			if(fd <= 2 || fd == APP_READ_FD || fd == APP_WRITE_FD)
-				// Skip stdin/out/err and the command pipe
-				return Nullable!FileDescriptor();
-			
-			string link = readLink(dir.name);
-			
-			auto linkMatch = link.matchFirst(specialLinkRE);
-			if(linkMatch) {
-				stderr.writeln("! File descriptor %d won't be saved: doesn't point to a file (it's a %s)"
-					.format(fd, linkMatch[1] == "anon_inode" ? linkMatch[2] : linkMatch[1]));
-				return Nullable!FileDescriptor();
-			}
-			
-			if(!link.isFile) {
-				stderr.writeln("! File descriptor %d won't be saved: file %s is not a regular file".format(fd, link));
+	return getFileDescriptors(pid)
+		.map!(delegate(fd) {
+			auto link = canSave(pid, fd);
+			if(!link[0]) {
+				stderr.writeln("! File descriptor %d won't be saved: %s".format(fd, link[1]));
 				return Nullable!FileDescriptor();
 			}
 			
@@ -51,7 +39,7 @@ auto readFiles(pid_t pid) {
 			
 			FileDescriptor file = {
 				descriptor: fd,
-				fileName: link,
+				fileName: link[1],
 				pos: fdInfoMatch[1].to!ulong,
 				flags: fdInfoMatch[2].to!int(8),
 			};
@@ -66,8 +54,18 @@ auto readFiles(pid_t pid) {
 /// stdin/out/err and the command pipes are skipped.
 void loadFiles(Range)(auto ref ProcInfo proc, Range newFiles)
 if(isInputRange!Range && is(ElementType!Range : FileDescriptor)) {
-	getFileDescriptors(proc.pid)
+	auto pid = proc.pid;
+	getFileDescriptors(pid)
+		.filter!(delegate(fd) {
+			auto link = canSave(pid, fd);
+			if(!link[0]) {
+				stderr.writeln("! File descriptor %d won't be closed: %s".format(fd, link[1]));
+				return false;
+			}
+			return true;
+		})
 		.each!((int fd) => proc.write(Wrapper2AppCmd.CMD_CLOSE, fd));
+	
 	newFiles
 		.each!(file => proc.write(
 			Wrapper2AppCmd.CMD_OPEN,
@@ -85,4 +83,17 @@ auto getFileDescriptors(pid_t pid) {
 		.map!(x => x.name.findSplitAfter("/fd/")[1].to!int)
 		.filter!(x => !(x <= 2 || x == APP_READ_FD || x == APP_WRITE_FD))
 	;
+}
+
+private Tuple!(bool, string) canSave(pid_t pid, int fd) {
+	string link = readLink("/proc/%d/fd/%d".format(pid, fd));
+	
+	auto linkMatch = link.matchFirst(specialLinkRE);
+	if(linkMatch)
+		return tuple(false, "doesn't point to a file, it's a "~(linkMatch[1] == "anon_inode" ? linkMatch[2] : linkMatch[1]));
+	
+	if(!link.isFile)
+		return tuple(false, "points to "~link~" which is not a regular file");
+	
+	return tuple(true, link);
 }
