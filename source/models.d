4d3b30254a7ddc87788c66759f38edd71f3582a4
Alex Parrill <initrd.gz@gmail.com>
2015-09-28 12:44:46 -0400

    Added to/fromTuple for models, which will be later used in serialization.

--- source/models.d
+++ source/models.d
@@ -4,8 +4,20 @@ module models;
 import std.algorithm;
 import std.range;
 import std.typecons;
+import std.exception;
 
 import bindings.ptrace : user_regs_struct, user_fpregs_struct;
+
+private ubyte[] struct2blob(T)(auto ref const(T) t)
+if(is(T == struct)) {
+	return (cast(ubyte*) (&t))[0..T.sizeof].dup;
+}
+
+private T blob2struct(T)(const(ubyte)[] blob)
+if(is(T == struct)) {
+	assert(blob.length == T.sizeof);
+	return *(cast(const(T)*) (blob.ptr));
+}
 
 /// Clock entry
 struct Clock {
@@ -13,7 +25,7 @@ struct Clock {
 }
 
 /// Save state
-class SaveState {
+final class SaveState {
 	/// ID of state. Null if the state isn't saved.
 	Nullable!(ulong, 0) id;
 	
@@ -52,10 +64,38 @@ class SaveState {
 		return dataSegment.front.end;
 	}
 	
-	auto toTuple() {
-		return tuple(id, name,
-			
+	alias ReprTuple = Tuple!(
+		//ulong, "id",
+		string, "name",
+		const(ubyte)[], "registers",
+		ulong, "realtime_sec",
+		ulong, "realtime_nsec",
+		ulong, "monotonic_sec",
+		ulong, "monotonic_nsec",
+		Nullable!uint, "windowSize_x",
+		Nullable!uint, "windowSize_y",
+	);
+	ReprTuple toTuple() {
+		return ReprTuple(name, registers.struct2blob, realtime.sec, realtime.nsec, monotonic.sec, monotonic.nsec,
+			windowSize.isNull ? Nullable!uint() : Nullable!uint(windowSize.get[0]),
+			windowSize.isNull ? Nullable!uint() : Nullable!uint(windowSize.get[1]),
 		);
+	}
+	static typeof(this) fromTuple(ulong thisId, ReprTuple tup) {
+		assert(tup.windowSize_x.isNull == tup.windowSize_y.isNull);
+		
+		auto state = new SaveState();
+		with(state) {
+			id = thisId;
+			name = tup.name;
+			registers = tup.registers.blob2struct!Registers,
+			realtime.sec = tup.realtime_sec,
+			realtime.nsec = tup.realtime_nsec,
+			monotonic.sec = tup.monotonic_sec,
+			monotonic.nsec = tup.monotonic_nsec,
+			windowSize = tup.windowSize_x.isNull ? typeof(windowSize)() : typeof(windowSize)(tuple(tup.windowSize_x.get, tup.windowSize_y.get));
+		}
+		return state;
 	}
 }
 
@@ -68,7 +108,7 @@ enum MemoryMapFlags {
 }
 
 /// Memory map entry
-class MemoryMap {
+final class MemoryMap {
 	/// ID of the memory map. Null if the map isn't saved.
 	Nullable!(ulong, 0) id;
 	
@@ -93,10 +133,37 @@ class MemoryMap {
 		assert(end >= begin);
 		assert(!contents || contents.length == (end - begin));
 	}
+	
+	alias ReprTuple = Tuple!(
+		SaveState, "state",
+		ulong, "begin",
+		ulong, "end",
+		uint, "flags",
+		string, "name",
+		ulong, "offset",
+		const(ubyte)[], "contents",
+	);
+	
+	ReprTuple toTuple(SaveState parent) {
+		assert(parent.maps.canFind(this));
+		return ReprTuple(parent, begin, end, flags, name, offset, contents);
+	}
+	typeof(this) fromTuple(ulong thisId, ReprTuple tup) {
+		auto map = new MemoryMap();
+		with(map) {
+			id = thisId;
+			begin = tup.begin;
+			end = tup.end;
+			flags = tup.flags;
+			name = tup.name,
+			contents = tup.contents;
+		}
+		return map;
+	}
 }
 
 /// File descriptor entry
-class FileDescriptor {
+final class FileDescriptor {
 	/// ID of the file. Null if the file isn't saved.
 	Nullable!(ulong, 0) id;
 	
@@ -111,6 +178,30 @@ class FileDescriptor {
 	
 	/// File open flags
 	int flags;
+	
+	alias ReprTuple = Tuple!(
+		SaveState, "state",
+		int, "descriptor",
+		string, "fileName",
+		ulong, "pos",
+		int, "flags"
+	);
+	
+	ReprTuple toTuple(SaveState parent) {
+		assert(parent.files.canFind(this));
+		return ReprTuple(parent, descriptor, fileName, pos, flags);
+	}
+	typeof(this) fromTuple(ulong thisId, ReprTuple tup) {
+		auto map = new FileDescriptor();
+		with(map) {
+			id = thisId;
+			descriptor = tup.descriptor;
+			fileName = tup.fileName;
+			pos = tup.pos;
+			flags = tup.flags;
+		}
+		return map;
+	}
 }
 
 /// Holds the contents of the (architecture dependent) registers.
